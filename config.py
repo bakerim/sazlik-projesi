3e2b22bc1da3dd9340d8e7efc23d4b879948cc98
bakerim <49491002+bakerim@users.noreply.github.com>
2025-12-11 22:13:53 +0300

    config.py

--- config.py
+++ config.py
@@ -1,27 +1,109 @@
 # config.py
 
-# Takip edilecek Haber Kaynakları (RSS)
+# --- 1. SİSTEM AYARLARI ---
 RSS_URLS = [
     "https://finance.yahoo.com/news/rssindex",
     "http://feeds.marketwatch.com/marketwatch/marketpulse",
-    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664"
+    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664",
+    "https://www.cnbc.com/id/100003114/device/rss/rss.html" # Ekstra global haber akışı
 ]
 
-# Takip Listesi ve İsim Eşleştirmeleri
+OUTPUT_FILE = "sazlik_signals.csv"
+
+# --- 2. ANA İZLEME LİSTESİ (WATCHLIST) ---
+# Bot bu Ticker'lar için piyasa verisi çekecek.
+WATCHLIST_TICKERS = [
+    "AAPL", "MSFT", "GOOGL", "GOOG", "AMZN", "NVDA", "META", "TSLA", "AVGO", "ADBE", 
+    "CRM", "CMCSA", "QCOM", "TXN", "AMGN", "INTC", "CSCO", "VZ", "T", "TMUS",
+    "NFLX", "ORCL", "MU", "IBM", "PYPL", "INTU", "AMD", "FTNT", "ADI", "NOW",
+    "LRCX", "MRVL", "CDNS", "SNPS", "DXCM", "KLAC", "ROST", "ANSS", "MSCI", "CHTR",
+    
+    # --- FİNANS & FİNANSAL HİZMETLER ---
+    "JPM", "V", "MA", "BAC", "WFC", "GS", "MS", "SPY", "BLK", "SCHW",
+    "C", "AXP", "CB", "MMC", "AON", "CME", "ICE", "PGR", "ALL", "MET",
+    "AIG", "PNC", "USB", "BK", "COF", "DFS", "TRV", "MCO", "CBOE", "RJF",
+    "GPN", "FIS", "ZION", "FITB", "STT", "NDAQ", "RF", "KEY", "CFG", "HBAN",
+    
+    # --- SAĞLIK & İLAÇ ---
+    "JNJ", "LLY", "UNH", "ABBV", "MRK", "PFE", "DHR", "TMO", "MDT", "SYK",
+    "AMGN", "GILD", "BIIB", "VRTX", "BMY", "ISRG", "ABT", "ZTS", "BDX", "BSX",
+    "CI", "CVS", "HUM", "HCA", "ANTM", "LH", "COO", "ALGN", "HOLX", "DVA",
+    "WAT", "RGEN", "IQV", "REGN", "EW", "TECH", "PKI", "DGX", "INCY", "CRL",
+    
+    # --- TEMEL TÜKETİM & DAYANIKLI TÜKETİM ---
+    "PG", "KO", "PEP", "WMT", "COST", "HD", "MCD", "NKE", "LOW", "TGT",
+    "SBUX", "MDLZ", "CL", "PM", "MO", "KR", "DG", "ADBE", "EL", "KHC",
+    "GIS", "K", "SYY", "APO", "DECK", "BBY", "WHR", "NWSA", "FOXA", "HAS",
+    "MAT", "HOG", "GT", "TIF", "TPR", "TTC", "VFC", "HBI", "KSS", "ULTA",
+    
+    # --- ENERJİ & SANAYİ ---
+    "XOM", "CVX", "BRK.B", "LMT", "RTX", "BA", "HON", "MMM", "GE", "GD",
+    "CAT", "DE", "EOG", "OXY", "SLB", "COP", "PSX", "MPC", "WMB", "KMI",
+    "ETN", "AOS", "EMR", "PCAR", "ROK", "SWK", "TDY", "RSG", "WM", "CARR",
+    "ITW", "GWW", "WAB", "IEX", "AAL", "DAL", "UAL", "LUV", "HA", "ALK",
+    
+    # --- EMLAK, KAMU HİZMETLERİ & DİĞER ---
+    "DUK", "NEE", "SO", "EXC", "AEP", "SRE", "WEC", "D", "ED", "XEL",
+    "VNQ", "SPG", "PLD", "EQIX", "AMT", "CCI", "HST", "O", "ARE", "PSA",
+    "WY", "BXP", "REG", "VTR", "AVB", "ESR", "EPR", "KIM", "FRT", "APTS",
+    "LUMN", "VIAC", "FOX", "DISCA", "ETSY", "EBAY", "ATVI", "EA", "TTWO", "ZG",
+    
+    # --- YARI İLETKEN & BULUT BİLİŞİM ---
+    "ASML", "AMAT", "TSM", "MCHP", "TER", "U", "VEEV", "OKTA", "NET", "CRWD", 
+    "DDOG", "ZS", "TEAM", "ADSK", "MSI", "FTV", "WDC", "ZBRA", "SWKS", "QDEL",
+    
+    # --- YENİLENEBİLİR ENERJİ & EV (Elektrikli Araçlar) ---
+    "FSLY", "PLUG", "ENPH", "SEDG", "RUN", "SPWR", "BLDP", "FCEL", "BE", "SOL",
+    "LI", "NIO", "XPEV", "RIVN", "LCID", "NKLA", "WKHS", "QS", "ARVL", "GOEV",
+    
+    # --- FİNANSAL TEKNOLOJİ (FinTech) & Dijital Ödeme ---
+    "SQ", "COIN", "HOOD", "UPST", "AFRM", "SOFI", "MQ", "BILL", "TOST", "PAYA",
+    "DWAC", "BRZE", "AVLR", "DOCU", "SABR", "TTEC", "TWLO", "RNG", "ZM", "COUP",
+    
+    # --- BİYOTEKNOLOJİ & SAĞLIK (Yüksek Büyüme) ---
+    "MRNA", "PFE", "BIIB", "VRTX", "REGN", "GILD", "AMGN", "BMRN", "ALXN", "CTAS",
+    "CORT", "EXEL", "IONS", "XBI", "LABU", "EDIT", "BEAM", "NTLA", "CRSP", "ALLK",
+    
+    # --- E-TİCARET & YENİ MEDYA ---
+    "MELI", "ETSY", "ROKU", "PTON", "SPOT", "CHWY", "ZM", "DOCU", "DDOG", "FVRR",
+    "PINS", "SNAP", "TWTR", "WIX", "SHOP", "SE", "BABA", "JD", "BIDU", "PDD",
+    
+    # --- ENDÜSTRİ & OTOMASYON ---
+    "ROP", "TT", "Ametek", "FLR", "HUBB", "APH", "ECL", "SHW", "PPG", "FMC",
+    "MOS", "CF", "NUE", "STLD", "ALK", "AAL", "DAL", "LUV", "UAL", "SAVE",
+    "CAR", "RCL", "CCL", "NCLH", "MGM", "WYNN", "LVS", "PENN", "DKNG", "BYND",
+    
+    # --- ÇEŞİTLİ DİNAMİK BÜYÜME ---
+    "RBLX", "UBER", "LYFT", "ABNB", "DOX", "GPN", "FLT", "PRU", "MET", "L",
+    "VLO", "PSX", "MPC", "DVN", "APA", "MRO", "EOG", "OXY", "SLB", "HAL",
+    "BKR", "FTI", "NOV", "TDW", "PAGP", "ENLC", "PAA", "WES", "WMB", "KMI",
+    "ETN", "AOS", "EMR", "PCAR", "ROK", "SWK", "TDY", "RSG", "WM", "CARR"
+]
+
+# --- 3. HABER KELİME EŞLEŞTİRMESİ (BOTUN ARADIĞI KELİMELER) ---
+# Bu sözlük, haber metnindeki şirket isimlerini Ticker koduna eşler.
+# Sadece en popüler olanları tutuyoruz, geri kalanı Ticker koduyla denenecek.
 TRACKED_STOCKS = {
-    # Teknoloji Devleri
     "apple": "AAPL", "microsoft": "MSFT", "google": "GOOGL", "alphabet": "GOOGL",
     "amazon": "AMZN", "nvidia": "NVDA", "meta": "META", "facebook": "META",
-    "tesla": "TSLA",
-    # Çip ve Donanım
-    "broadcom": "AVGO", "qualcomm": "QCOM", "intel": "INTC", "amd": "AMD",
-    "micron": "MU", "texas instruments": "TXN", "analog devices": "ADI",
-    # Yazılım ve Servisler
-    "adobe": "ADBE", "salesforce": "CRM", "oracle": "ORCL", "ibm": "IBM",
-    "paypal": "PYPL", "netflix": "NFLX",
-    # Diğer
-    "cisco": "CSCO", "at&t": "T", "disney": "DIS"
+    "tesla": "TSLA", "broadcom": "AVGO", "adobe": "ADBE", "salesforce": "CRM",
+    "qualcomm": "QCOM", "amgen": "AMGN", "intel": "INTC", "cisco": "CSCO",
+    "verizon": "VZ", "att": "T", "netflix": "NFLX", "oracle": "ORCL",
+    "ibm": "IBM", "paypal": "PYPL", "intuit": "INTU", "amd": "AMD",
+    "morgan stanley": "MS", "jpmorgan": "JPM", "visa": "V", "mastercard": "MA",
+    "bank of america": "BAC", "pfizer": "PFE", "johnson & johnson": "JNJ",
+    "walmart": "WMT", "coca cola": "KO", "pepsi": "PEP", "mcdonalds": "MCD",
+    "nike": "NKE", "starbucks": "SBUX", "exxon mobil": "XOM", "chevron": "CVX",
+    "berkshire hathaway": "BRK.B", "lockheed martin": "LMT", "raytheon": "RTX",
+    "boeing": "BA", "general electric": "GE", "general dynamics": "GD",
+    "caterpillar": "CAT", "netflix": "NFLX", "spotify": "SPOT", "uber": "UBER",
+    "lyft": "LYFT", "airbnb": "ABNB", "etsy": "ETSY", "roku": "ROKU",
+    "coinbase": "COIN", "shopify": "SHOP", "alibaba": "BABA", "jd com": "JD"
 }
 
+# Ticker kodlarını da kelime olarak ekleyelim (NVDA, MSFT vb. direkt geçebilir)
+for ticker in WATCHLIST_TICKERS:
+    if ticker.lower() not in TRACKED_STOCKS:
+        TRACKED_STOCKS[ticker.lower()] = ticker
 # Sonuçların Kaydedileceği Dosya Adı
 OUTPUT_FILE = "sazlik_signals.csv"
